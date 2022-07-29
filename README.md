39bf049d2c927b98bf7edd6fb4b364e4fca288e1
Brandon Long <brandon@booksimplify.com>
2022-07-29 18:25:10 -0500

    Added LinkedHashMap in Rust.

---
+++ README.md
@@ -0,0 +1,21 @@
+# Rust LinkedHashMap
+LinkedHashMap implementation in Rust.
+
+## About
+This is certainly not the most efficient or *best* implementation; however, I'm
+mainly just trying to learn rust docs, unit testing, and generics. The code I have now
+should allow for reverse lookup by index in the future, so that's pretty cool.
+
+## Getting Started
+- [Install Rust](https://www.rust-lang.org/tools/install)
+
+## Generate Documentation
+- Run `cargo doc`
+- Open `./target/doc/linkedhashmap/index.html` in a web browser
+
+## Run Tests
+`cargo test`
+
+## TODO
+- Make this even more non-standard and do reverse lookup by index?
+- 
---
+++ src/lib.rs
@@ -0,0 +1,246 @@
+#![crate_name = "linkedhashmap"]
+#![allow(dead_code)]
+
+//! Provides an easy interface to preserve the insertion order of your `HashMap`.
+
+/// `LinkedHashMap` data structure lives here.
+pub mod ds {
+    use core::fmt;
+    use std::collections::HashMap;
+    use std::cmp::Eq;
+    use std::fmt::Debug;
+    use std::hash::Hash;
+    use std::marker::Copy;
+
+    /// Stores an index for quick key lookup and the value.
+    struct LinkedHashMapValue<V> {
+        _index: usize,
+        _value: V,
+    }
+
+    /// Stores number of keys, keys in order, and values.
+    pub struct LinkedHashMap<K, V> {
+        _len: usize,
+        _keys: Vec<K>,
+        _values: HashMap<K, LinkedHashMapValue<V>>,
+    }
+    
+    impl<K, V> LinkedHashMap<K, V> where K: Eq + Hash + Copy, V: Copy {
+        /// Creates new `LinkedHashMap`.
+        pub fn new() -> Self {
+            return LinkedHashMap {
+                _len: 0,
+                _keys: Vec::new(),
+                _values: HashMap::new(),
+            };
+        }
+
+        /// Gets value; returns `Some(v)` if exists or `None`.
+        pub fn get(&self, k: K) -> Option<V> {
+            let value = self._values.get(&k);
+            if value.is_none() {
+                return None;
+            }
+            
+            return Some(value.unwrap()._value);
+        }
+
+        /// Sets value; upserts if exists already or adds new entry.
+        pub fn set(&mut self, k: K, v: V) {
+            if self._values.contains_key(&k) {
+                let value: &LinkedHashMapValue<V> = self._values.get(&k).unwrap();
+                self._values.insert(k, LinkedHashMapValue {
+                    _index: value._index,
+                    _value: v,
+                });
+            } else {
+                self._keys.push(k);
+                self._values.insert(k, LinkedHashMapValue {
+                    _index: self._len,
+                    _value: v,
+                });
+                self._len += 1;
+            }
+        }
+
+        /// Removes value; returns `Some(v)` if exists or `None`.
+        pub fn remove(&mut self, k: K) -> Option<V> {
+            if self._values.contains_key(&k) {
+                let removed: LinkedHashMapValue<V> = self._values.remove(&k).unwrap();
+                self._keys.remove(removed._index);
+                self._len -= 1;
+                
+                return Some(removed._value);
+            }
+
+            return None;
+        }
+
+        /// Clears all values.
+        pub fn clear(&mut self) {
+            self._keys.clear();
+            self._values.clear();
+            self._len = 0;
+        }
+        
+        /// Gets length.
+        pub fn len(&self) -> usize {
+            return self._len;
+        }
+
+        /// Check if contains a key.
+        pub fn contains_key(&self, k: K) -> bool {
+            return self._values.contains_key(&k);
+        }
+
+        /// Gets all keys.
+        pub fn keys(&self) -> Vec<K> {
+            return self._keys.clone();
+        }
+
+        /// Gets all values.
+        pub fn values(&self) -> Vec<V> {
+            let mut rvs: Vec<V> = Vec::new();
+            for k in self._keys.iter() {
+                rvs.push(self._values.get(k).unwrap()._value);   
+            }
+            return rvs;
+        }
+
+        /// Iterator.
+        pub fn iter(&self) -> impl Iterator<Item=(K, V)> + '_ {
+            let mut rvs: Vec<(K, V)> = Vec::new();
+            for k in self._keys.iter() {
+                rvs.push((*k, self._values.get(k).unwrap()._value));   
+            }
+            return rvs.into_iter();
+        }
+    }
+
+    impl<K, V> fmt::Debug for LinkedHashMap<K, V> where K: Eq + Hash + Copy + Debug, V: Copy + Debug {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            let mut out: String = String::new();
+            for (k, v) in self.iter() {
+                out += format!("{:?}: {:?}", k, v).as_str();
+            }
+
+            return write!(f, "{}", out);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    mod linked_hashmap {
+        mod linked_hashmap {
+            use crate::ds::*;
+    
+            #[test]
+            fn new() {
+                let ins = self::LinkedHashMap::<&str, usize>::new();
+                assert!(ins.len() == 0);
+                assert!(ins.keys().len() == 0);
+                assert!(ins.values().len() == 0);
+            }
+
+            #[test]
+            fn get() {
+                let mut ins = self::LinkedHashMap::<&str, usize>::new();
+                assert!(ins.get("k") == None);
+                ins.set("k", 1);
+                assert!(ins.get("k") == Some(1));
+            }
+            
+            #[test]
+            fn set() {
+                let mut ins = self::LinkedHashMap::<&str, usize>::new();
+                ins.set("k", 1);
+                assert!(ins.len() == 1);
+                assert!(ins.keys().len() == 1);
+                assert!(ins.values().len() == 1);
+                assert!(ins.get("k") == Some(1));
+            }
+            
+            #[test]
+            fn remove() {
+                let mut ins = self::LinkedHashMap::<&str, usize>::new();
+                assert!(ins.remove("k") == None);
+                assert!(ins.len() == 0);
+                assert!(ins.keys().len() == 0);
+                assert!(ins.values().len() == 0);
+                ins.set("k", 1);
+                assert!(ins.remove("k") == Some(1));
+                assert!(ins.len() == 0);
+                assert!(ins.keys().len() == 0);
+                assert!(ins.values().len() == 0);
+            }
+            
+            #[test]
+            fn clear() {
+                let mut ins = self::LinkedHashMap::<&str, usize>::new();
+                ins.clear();
+                assert!(ins.len() == 0);
+                assert!(ins.keys().len() == 0);
+                assert!(ins.values().len() == 0);
+                ins.set("k", 1);
+                ins.clear();
+                assert!(ins.len() == 0);
+                assert!(ins.keys().len() == 0);
+                assert!(ins.values().len() == 0);
+            }
+            
+            #[test]
+            fn len() {
+                let mut ins = self::LinkedHashMap::<&str, usize>::new();
+                assert!(ins.len() == 0);
+                ins.clear();
+                assert!(ins.len() == 0);
+                ins.set("k", 1);
+                assert!(ins.len() == 1);
+                ins.clear();
+                assert!(ins.len() == 0);
+            }
+            
+            #[test]
+            fn contains_key() {
+                let mut ins = self::LinkedHashMap::<&str, usize>::new();
+                assert!(ins.contains_key("k") == false);
+                ins.set("k", 1);
+                assert!(ins.contains_key("k") == true);
+            }
+            
+            #[test]
+            fn keys() {
+                let mut ins = self::LinkedHashMap::<&str, usize>::new();
+                let mut keys: Vec<&str> = Vec::new();
+                assert!(ins.keys().eq(&keys));
+                ins.set("k", 1);
+                keys.push("k");
+                assert!(ins.keys().eq(&keys));
+            }
+            
+            #[test]
+            fn values() {
+                let mut ins = self::LinkedHashMap::<&str, usize>::new();
+                let mut values: Vec<usize> = Vec::new();
+                assert!(ins.values().eq(&values));
+                ins.set("k", 1);
+                values.push(1);
+                assert!(ins.values().eq(&values));
+            }
+        }
+
+        mod debug {
+            use crate::ds::*;
+
+            #[test]
+            fn fmt() {
+                let mut ins = self::LinkedHashMap::<&str, usize>::new();
+                assert!("" == format!("{:?}", ins));
+                ins.set("k", 1);
+                println!("{:?}", ins);
+                assert!("\"k\": 1" == format!("{:?}", ins));
+            }
+        }
+    }
+}
